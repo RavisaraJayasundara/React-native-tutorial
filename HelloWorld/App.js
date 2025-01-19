e53c4ce1fbd07b075a2a0fa749e98d71e2734d00
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-01-19 21:33:52 +0530

    add Images

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image, ImageBackground} from 'react-native';
+const  flowerImg = require("./assets/image/flower.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Ravi</Text>
-      <StatusBar style="auto" />
+    {/* <Text style={{color:"white"}}>Hello Ravi</Text>
+     <Image source={flowerImg} style={{width:350,height:300}}/>
+     <Image source={{uri:"https://picsum.photos/id/237/200/300"}} style={{width:350,height:300}}/>
+      */}<StatusBar style="auto" />
+      <ImageBackground source={flowerImg} style={{flex:1}}>
+        <Text>Image Text</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -14,7 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    backgroundColor:'plum'
   },
 });
